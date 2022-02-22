be1ebfe8aeae051ff8511814822689baef797cd9
yugal1005 <yugalpanchal01@gmail.com>
2022-02-22 14:05:29 +0530

    Updated Auth Screen

--- lib/screens/auth_screen_new_ui.dart
+++ lib/screens/auth_screen_new_ui.dart
@@ -3,21 +3,45 @@ import 'package:flutter/material.dart';
 class NewAuthScreen extends StatelessWidget {
   final googleImg = Image.asset("assets/Img/google.png");
   final facebookImg = Image.asset("assets/Img/facebook.png");
-  Widget buildInputField(String displayText) {
-    return Container(
-      margin: const EdgeInsets.only(left: 15, right: 15),
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: TextField(
-        decoration: InputDecoration(
-          border: InputBorder.none,
-          hintText: displayText,
-        ),
-      ),
-    );
+  Widget buildInputField(String displayText, bool pass) {
+    return pass
+        ? Container(
+            margin: const EdgeInsets.only(left: 15, right: 15),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Form(
+              child: TextFormField(
+                style: TextStyle(color: Colors.black87),
+                obscureText: true,
+                // onFieldSubmitted: (value) => next,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: displayText,
+                ),
+              ),
+            ),
+          )
+        : Container(
+            margin: const EdgeInsets.only(left: 15, right: 15),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Form(
+              child: TextFormField(
+                style: TextStyle(color: Colors.black87),
+                // onFieldSubmitted: (value) => next,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: displayText,
+                ),
+              ),
+            ),
+          );
   }
 
   @override
@@ -25,18 +49,19 @@ class NewAuthScreen extends StatelessWidget {
     return Scaffold(
       // backgroundColor: Color.lerp(Color.fromRGBO(139, 199, 255, 0.4),
       //     Color.fromRGBO(139, 199, 255, 1), 0.7),
-      body: SingleChildScrollView(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color.fromRGBO(139, 199, 255, 0.7),
-                Color.fromRGBO(139, 199, 255, 1),
-              ],
-            ),
+      body: Container(
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromRGBO(139, 199, 255, 0.7),
+              Color.fromRGBO(139, 199, 255, 1),
+            ],
           ),
+        ),
+        child: SingleChildScrollView(
           child: Column(
             // mainAxisAlignment: MainAxisAlignment.,
             // crossAxisAlignment: CrossAxisAlignment.center,
@@ -69,11 +94,11 @@ class NewAuthScreen extends StatelessWidget {
               const SizedBox(
                 height: 7,
               ),
-              buildInputField("Email"),
+              buildInputField("Email", false),
               const SizedBox(
                 height: 7,
               ),
-              buildInputField("Password"),
+              buildInputField("Password", true),
               const SizedBox(
                 height: 7,
               ),
@@ -114,14 +139,17 @@ class NewAuthScreen extends StatelessWidget {
                 child: FlatButton(
                   onPressed: () {},
                   child: ListTile(
-                    leading: Image.network(
-                      "https://ibb.co/kM0J0xH",
-                      fit: BoxFit.contain,
-                      loadingBuilder: (context, child, loadingProgress) =>
-                          (loadingProgress == null)
-                              ? child
-                              : CircularProgressIndicator(),
-                      errorBuilder: (context, error, stackTrace) => googleImg,
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: Image.network(
+                        "https://ibb.co/kM0J0xH",
+                        fit: BoxFit.contain,
+                        loadingBuilder: (context, child, loadingProgress) =>
+                            (loadingProgress == null)
+                                ? child
+                                : CircularProgressIndicator(),
+                        errorBuilder: (context, error, stackTrace) => googleImg,
+                      ),
                     ),
                     title: Text(
                       "Continue with Google",
@@ -144,14 +172,20 @@ class NewAuthScreen extends StatelessWidget {
                 child: FlatButton(
                   onPressed: () {},
                   child: ListTile(
-                    leading: Image.network(
-                      "https://ibb.co/qrt6k2R",
-                      fit: BoxFit.contain,
-                      loadingBuilder: (context, child, loadingProgress) =>
-                          (loadingProgress == null)
-                              ? child
-                              : CircularProgressIndicator(),
-                      errorBuilder: (context, error, stackTrace) => facebookImg,
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: Image.network(
+                        "https://ibb.co/qrt6k2R",
+                        fit: BoxFit.contain,
+                        loadingBuilder: (context, child, loadingProgress) =>
+                            (loadingProgress == null)
+                                ? child
+                                : CircularProgressIndicator(
+                                    color: Colors.black87,
+                                  ),
+                        errorBuilder: (context, error, stackTrace) =>
+                            facebookImg,
+                      ),
                     ),
                     title: Text(
                       "Continue with Facebook",
@@ -160,12 +194,11 @@ class NewAuthScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 7,
-              ),
-              Container(
-                // padding: EdgeInsets.only(left: 30),
-                // alignment: Alignment.bottomLeft,
+              // const SizedBox(
+              //   height: 7,
+              // ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
                 child: Row(
                   children: [
                     Text(
@@ -176,12 +209,9 @@ class NewAuthScreen extends StatelessWidget {
                   ],
                 ),
               ),
-              const SizedBox(
-                height: 5,
-              ),
               Container(
-                // padding: EdgeInsets.only(left: 30),
-                // alignment: Alignment.bottomLeft,
+                alignment: Alignment.topLeft,
+                // padding: const EdgeInsets.only(left: 15),
                 child: FlatButton(
                     onPressed: () {}, child: Text("Forget your password")),
               ),
